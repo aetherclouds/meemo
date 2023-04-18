3cae43ca421589292f3ecaa57a658e0972302250
Erik <androydgba@gmail.com>
2023-04-18 17:37:38 -0300

    updated version as well

--- generate_manifest.js
+++ generate_manifest.js
@@ -5,7 +5,7 @@ export default function generateExtensionManifest(targetBrowser) {
 
         "name": "meemo",
         "description": "A couple versatile tools aimed at helping language learners!",
-        "version": "0.0.0.1",
+        "version": "0.0.0.2",
         
         "icons": {
             "48": "assets/icons/ext-icon48.png",
